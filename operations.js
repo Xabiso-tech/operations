8e38e87d965ceb7c79b8a2200551a63cbe2d9d8b
Xabiso-tech <xphendu1@gmail.com>
2026-05-21 09:04:45 +0200

    Add files via upload

---
+++ operations.js
@@ -0,0 +1,14 @@
+let  number1 = 20;
+let  number2 = 5;
+
+let number1string = String(number1);
+
+let sum = number1 + number2;
+
+let product = number1 * number2;
+
+console.log("Number 1: ", number1);
+console.log("Number 2: ", number2);
+console.log("Number 1 as a string: ", number1string);
+console.log("Sum: ", sum);
+console.log("Product: ", product);
